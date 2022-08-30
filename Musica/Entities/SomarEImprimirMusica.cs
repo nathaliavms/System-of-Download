840a9eee7ad694e60fd58f177e23e510649ef39e
Nathalia Vieira <Silva.Nathalia@ibm.com>
2022-08-30 18:36:40 -0300

    first commit

---
+++ Musica/Entities/SomarEImprimirMusica.cs
@@ -0,0 +1,45 @@
+﻿namespace Musica.Entities
+{
+    class SomarNumeroInteiro
+    {
+        private int Botao1 { get; set; }
+        private int Botao2 { get; set; }
+        private int Soma { get; set; }
+        public SomarNumeroInteiro(int botao1, int botao2)
+        {
+            Botao1 = botao1;
+            Botao2 = botao2;
+        }
+        public string CalcularSoma()
+        {
+            Soma = Botao1 + Botao2;
+            switch (Soma)
+            {
+                case 0:
+                    return "PROXYCITY";
+                case 1:
+                    return "P.Y.N.G.";
+                case 2:
+                    return "DNSUEY!";
+                case 3:
+                    return "SERVERS";
+                case 4:
+                    return "HOST!";
+                case 5:
+                    return "CRIPTONIZE";
+                case 6:
+                    return "OFFLINE DAY";
+                case 7:
+                    return "SALT";
+                case 8:
+                    return "ANSWER!";
+                case 9:
+                    return "RAR?";
+                case 10:
+                    return "WIFI ANTENNAS";
+                default:
+                    return "Por favor digite dois numeros inteiros, sendo a soma dos dois até o limite 10";
+            }
+        }
+    }
+}
---
+++ Musica/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Musica.Entities;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Quantas vezes você vai querer escolher uma musica?");
+                var qtde = int.Parse(Console.ReadLine());
+                for (int i = 0; i < qtde; i++)
+                {
+                    Console.WriteLine("Digite dois numeros inteiros de 0 a 9: ");
+                    string[] numeros = Console.ReadLine().Split(' ');
+                    var botao1 = int.Parse(numeros[0]);
+                    var botao2 = int.Parse(numeros[1]);
+                    SomarNumeroInteiro calculo = new SomarNumeroInteiro(botao1, botao2);
+                    Console.WriteLine(calculo.CalcularSoma());
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro, por favor digite um numero inteiro");
+            }
+        }
+    }
+}
